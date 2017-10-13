24dd6fce25ea9fac81468d44007c4c9af17724a9
Simon Buckingham <simon@icecandy.com>
2017-10-13 15:12:14 +0100

    Updated article links

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TitaniumDelayedRenderingExample
 An example project in Titanium to show several techniques for delayed rendering and dynamic text block sizing
+
+You can read articles on this example on Medium.
+
+Part 1: https://medium.com/@simonbuckingham/delayed-rendering-and-dynamic-text-block-sizing-in-titanium-part-1-840be2b20826
+Part 2: https://medium.com/@simonbuckingham/delayed-rendering-and-dynamic-text-block-sizing-in-titanium-part-2-edb6ede7f34e
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,7 +4,9 @@
 //                example 1: using a simulated data load delay, we wait until the data has loaded before rendering a set of image and text tiles
 //                example 2: using a real-world download of remote images, as for example 1, we wait until the data has loaded before rendering a set of image and text tiles
 //                example 3: using a variable watcher technique, as for example 1, we wait until the data has loaded before rendering a set of image and text tiles
-
+//@reference      You can read articles on this example on Medium.
+//                Part 1: https://medium.com/@simonbuckingham/delayed-rendering-and-dynamic-text-block-sizing-in-titanium-part-1-840be2b20826
+//                Part 2: https://medium.com/@simonbuckingham/delayed-rendering-and-dynamic-text-block-sizing-in-titanium-part-2-edb6ede7f34e
 
 function onClickExample1(_evt) {
     Alloy.createController('example', {
